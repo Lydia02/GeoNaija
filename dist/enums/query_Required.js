3977bb102bdb2cfa1b0df1e82cf8c28a10c980b0
Lydia <ojoawolydia@gmail.com>
2023-06-23 01:09:23 +0200

    production stage

--- dist/enums/query_Required.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.InvalidQueryError = void 0;
-var InvalidQueryError;
-(function (InvalidQueryError) {
-    InvalidQueryError["REGION_ERROR"] = "region_name field is required";
-    InvalidQueryError["STATE_ERROR"] = "state_name field is required";
-    InvalidQueryError["LGA_ERROR"] = "lga_name field is required";
-})(InvalidQueryError || (exports.InvalidQueryError = InvalidQueryError = {}));
